1189e2a90b2d841248f6cccf3d8ccc42bc143289
Allison Snipes <ajsnipes@usfca.edu>
2019-11-11 14:50:50 -0800

    first commit

---
+++ formatstr.py
@@ -0,0 +1,15 @@
+my_name = 'Allison Juliette'
+my_age = 29
+my_height = 4.5*12
+my_weight = 250
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'black'
+
+print 'lets talk about %s' % my_name
+print 'i am %d inches tall' % my_height
+print 'i am %d heavy' % my_weight
+print 'its not that heavy'
+print 'my eyes are %s and my hair is %s' % (my_eyes, my_hair)
+print 'my teeth are %s' % my_teeth
+print 'if i add %d, %d, and %d I get %d' % (my_age, my_height, my_weight, my_age + my_height + my_weight)
